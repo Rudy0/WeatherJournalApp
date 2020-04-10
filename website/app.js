14f646f3d323fa9f83cddac53a80f9817430d600
Rudy0 <paduraru.rudolf.vasile@gmail.com>
2020-04-10 19:35:40 +0300

    setting up the server

--- website/app.js
+++ website/app.js
@@ -1,5 +1,20 @@
 /* Global Variables */
+let baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = '0252daeced35aad0d7a14eef14d3ced4';
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
